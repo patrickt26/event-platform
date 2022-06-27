2cda434f9a8d13d93e131720e9e5606056d3aea0
Patrick Thomaz <plthomaz@gmail.com>
2022-06-27 16:36:22 -0300

    Implemented responsive in Event page

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -36,12 +36,12 @@ export function Video(props: VideoProps) {
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto mb-10">
-        <div className="flex items-start gap-16">
+        <div className="flex items-start gap-16 flex-col sm:flex-row">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
+            <h1 className="text-lg sm:text-2xl font-bold">
               {data.lesson.title}
             </h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">
+            <p className="text-sm sm:text-base mt-4 text-gray-200 leading-relaxed">
               {data.lesson.description}
             </p>
 
@@ -54,14 +54,14 @@ export function Video(props: VideoProps) {
                 />
 
                 <div className="leading-relaxed">
-                  <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                  <strong className="font-bold text-lg sm:text-2xl block">{data.lesson.teacher.name}</strong>
                   <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
                 </div>
               </div>
             )}
           </div>
 
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 w-full sm:w-1/2 md:w-auto">
             <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
               <DiscordLogo size={24} />
               Comunidade do Discord
@@ -74,34 +74,34 @@ export function Video(props: VideoProps) {
           </div>
         </div>
 
-        <div className="gap-8 mt-20 grid grid-cols-2">
+        <div className="gap-8 mt-12 sm:mt-20 grid grid-cols-1 sm:grid-cols-2">
           <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40} />
             </div>
             <div className="py-6 leading-relaxed">
-              <strong className="text-2xl">Material complementar</strong>
-              <p className="text-sm text-gray-200 mt-2">
+              <strong className="text-lg sm:text-2xl">Material complementar</strong>
+              <p className="text-xs text-gray-200 mt-2">
                 Acesse o material complementar para acelerar o seu desenvolvimento
               </p>
             </div>
             <div className="h-full p-6 flex items-center">
-              <CaretRight size={24} />
+              <CaretRight color="#81D8F7" size={24} />
             </div>
           </a>
 
-          <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-4 sm:gap-6 hover:bg-gray-600 transition-colors">
             <div className="bg-green-700 h-full p-6 flex items-center">
               <Image size={40} />
             </div>
             <div className="py-6 leading-relaxed">
-              <strong className="text-2xl">Wallpapers exclusivos</strong>
-              <p className="text-sm text-gray-200 mt-2">
+              <strong className="text-lg sm:text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-xs text-gray-200 mt-2">
                 Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
               </p>
             </div>
             <div className="h-full p-6 flex items-center">
-              <CaretRight size={24} />
+              <CaretRight color="#81D8F7" size={24} />
             </div>
           </a>
         </div>
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Footer } from "../components/Footer";
@@ -23,7 +24,9 @@ export function Event() {
     <div className="flex flex-col min-h-screen">
       <Header toggleMenuParent={toggleMenu} />
       <main className="flex flex-1">
-        <div className="flex flex-col flex-1">
+        <div className={classNames('flex flex-col flex-1', {
+          'hidden': openSidebar,
+        })}>
           { slug ? <Video lessonSlug={slug} /> : <div className="flex-1" /> }
           <Footer />
         </div>
